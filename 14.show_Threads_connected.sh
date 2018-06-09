6289ff34b3786442a82192cc2831bf8539c90e21
Your Name <you@example.com>
2018-06-09 19:14:39 +0800

    mysql

---
+++ 14.show_Threads_connected.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#show processlist有问题 
+# |grep -v "Sleep|awk{print $1} ..."
+
+while [ TRUE ]; do
+	mysql -h127.0.0.1 -uroot -proot -BNe  "show status like 'Threads_connected';"
+	sleep 2
+done
+echo "finished"
--- 2.migrate_grant.sh
+++ 2.migrate_grant.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #迁移grant脚本
-for i in `/home/mysql/mysql/bin/mysql -hlocalhost -uroot -proot -BNe  "select concat(user,'@',host) from mysql.user"`
+for i in `mysql -hlocalhost -uroot -proot -BNe  "select concat(user,'@',host) from mysql.user"`
 do 
-	/home/mysql/mysql/bin/mysql -hlocalhost -uroot -proot -proot \
+	mysql -hlocalhost -uroot -proot -proot \
 	-BNe "show grants for ${i}"|awk '{print $0";"}'; 
 done
