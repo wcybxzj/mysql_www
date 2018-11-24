0085f44dbfc4c0debc23b48957f290f73f5a61ee
Your Name <you@example.com>
2018-11-24 15:34:10 +0800

    add last

--- 1.shang/3.show_processlist.sh
+++ 1.shang/3.show_processlist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #show processlist有问题 
 # |grep -v "Sleep|awk{print $1} ..."
-for i in `/home/mysql/mysql/bin/mysql -hlocalhost -uroot -proot -BNe  "show processlist;"`
+for i in `mysql -hlocalhost -uroot -proot -BNe  "show processlist;"`
 do 
 	echo ${i}
 done
